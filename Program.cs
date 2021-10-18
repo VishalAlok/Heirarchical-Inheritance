b68290d5939abaf1cccbdafac16e45206fcf827a
vishal.Alok <you@example.com>
2021-10-18 06:06:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Heirarchical_Inheritance
+{
+    class BaseClass
+    {
+        public void hello()
+        {
+            Console.WriteLine("Parent's Hello Method");
+        }
+    }
+    class ChildClass : BaseClass
+    {
+        public void World()
+        {
+            Console.WriteLine("Child's World Method");
+        }
+    }
+    class SecondChildClass : BaseClass
+    {
+        public void Hi()
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildClass c1 = new ChildClass();
+            SecondChildClass c2 = new SecondChildClass();
+            c1.hello();
+            c1.World();
+            c2.hello();
+            c2.Hi();
+        }
+    }
+}
